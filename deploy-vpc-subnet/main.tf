7988d75b4d59613c8837adae7a6c9cb9e2918213
Rohan Moite <rohanmohite3112@gmail.com>
2025-12-05 12:27:59 +0530

    vpc and subnet create

---
+++ deploy-vpc-subnet/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = us-east-1
+}
+
+# Create Custom VPC
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "custom-vpc"
+  }
+}
+
+# Create subnet without IGW or RT
+resource "aws_subnet" "subnet_no_igw" {
+  vpc_id    = aws_vpc.custom_vpc.id
+  cidr_block    = "10.0.1.0/24"
+  availability_zone = "us-east-1f"
+
+  tags = {
+    Name = "test-private-subnet"
+  }
+}
