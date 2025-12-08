36fa0d737021c2f24a93098b6fd08c18913d2f9f
Rohan Moite <rohanmohite3112@gmail.com>
2025-12-08 11:19:16 +0530

    terraform script created to create new user.

---
+++ create-iam-user/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers{
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an IAM user
+resource "aws_iam_user" "tester" {
+  name = "tester-user"
+  path = "/"
+}
+
+#Attach policy to user directly
+resource "aws_iam_user_policy" "tester_policy" {
+  name = "tester-inline-policy"
+  user = aws_iam_user.tester.name
+
+  policy = jsondecode({
+    Version = "2012-10-17",
+    Statement = [
+        {
+            Action = [
+                "ec2:Describe*",
+                "s3:ListAllMyBuckets"
+            ],
+            Effect = "Allow",
+            Resource = "*"
+        }
+    ]
+  })
+}
