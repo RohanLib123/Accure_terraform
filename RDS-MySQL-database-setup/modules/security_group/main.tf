7af72f9e7e7d5104441c7b0e55f4b8a1550e31c2
Rohan Moite <rohanmohite3112@gmail.com>
2025-12-11 17:41:14 +0530

    source security group id commented

--- RDS-MySQL-database-setup/modules/security_group/main.tf
+++ RDS-MySQL-database-setup/modules/security_group/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "this_sg" {
 }
 
 resource "aws_security_group_rule" "ingress" {
-  #for_each = { for idx, r in var.ingress_rules : idx => r }
+  for_each = { for idx, r in var.ingress_rules : idx => r }
   type = "ingress"
   from_port = each.value.from_port
   to_port = each.value.to_port
@@ -15,12 +15,12 @@ resource "aws_security_group_rule" "ingress" {
   security_group_id = aws_security_group.this_sg.id
 
   cidr_blocks = lookup(each.value, "cidr_blocks", [])
-  source_security_group_id = lookup(each.value, "source_sg_id", null)
+  #source_security_group_id = lookup(each.value, "source_sg_id", null)
   description = lookup(each.value, "description", "")
 }
 
 resource "aws_security_group_rule" "egress" {
-  #for_each = { for idx, r in var.egress_rules : idx => r} 
+  for_each = { for idx, r in var.egress_rules : idx => r} 
   type = "egress"
   from_port = each.value.from_port
   to_port = each.value.to_port
