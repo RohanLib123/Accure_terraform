7cf2588179a58ba07041e2610cc37e098bd75631
Rohan Moite <rohanmohite3112@gmail.com>
2025-12-11 18:38:07 +0530

    rebased main file of security module

--- RDS-MySQL-database-setup/modules/security_group/main.tf
+++ RDS-MySQL-database-setup/modules/security_group/main.tf
@@ -7,41 +7,16 @@ resource "aws_security_group" "this_sg" {
 }
 
 resource "aws_security_group_rule" "ingress" {
- # for_each = { for idx, r in var.ingress_rules : idx => r }
+  for_each = { for idx, r in var.ingress_rules : idx => r }
   type = "ingress"
   from_port = each.value.from_port
   to_port = each.value.to_port
-  protocol = each.value.protocol 
+  protocol = each.value.protocol
   security_group_id = aws_security_group.this_sg.id
 
-  #cidr_blocks = lookup(each.value, "cidr_blocks", [])
-  #source_security_group_id = lookup(each.value, "source_sg_id", null)
+  cidr_blocks = lookup(each.value, "cidr_blocks", [])
+  source_security_group_id = lookup(each.value, "source_sg_id", null)
   description = lookup(each.value, "description", "")
-
-  # ... other necessary arguments ...
-  
-  # 1. Define source_security_group_id:
-  # Check if a source_sg_id is provided in the input map/object.
-  source_security_group_id = (
-    lookup(each.value, "source_sg_id", "") != "" 
-    ? lookup(each.value, "source_sg_id", null) 
-    : null
-  )
-  
-  # 2. Define cidr_blocks:
-  # Use CIDR blocks ONLY if source_security_group_id is NOT provided (i.e., 'source_sg_id' is null or empty).
-  cidr_blocks = (
-    lookup(each.value, "source_sg_id", "") == ""
-    ? lookup(each.value, "cidr_blocks", []) # Fallback to CIDR blocks
-    : null # Set to null if an SG ID is provided
-  )
-}
-
-
-# You should define a local variable to simplify the check:
-locals {
-  # This evaluates to true if the source_sg_id field is present and non-empty
-  source_sg_id_is_present = lookup(var.ingress_rules[each.key], "source_sg_id", "") != ""
 }
 
 resource "aws_security_group_rule" "egress" {
